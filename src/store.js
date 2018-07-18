8048aed64b7f1b01de9a565db3c211731ec66888
Fawwaz Khayyat <fawaz.khayat@gmail.com>
2018-07-18 06:47:43 -0400

    Add links to store.js

--- src/store.js
+++ src/store.js
@@ -6,6 +6,11 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     title: 'My Custom Title',
+    links: [
+      'http://google.com',
+      'http://coursetro.com',
+      'http://youtube.com',
+    ],
   },
   mutations: {
 
